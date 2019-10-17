6450b311737059d62b4f3b9c2f4ebf11ea45f7c9
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-17 16:24:50 +0200

    Worked towards different usertypes

--- Lab5-WPF/Lab5-WPF/MainWindow.xaml.cs
+++ Lab5-WPF/Lab5-WPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Lab5_WPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DisLabel.Visibility = Visibility.Visible;
+        }
     }
 }
---
+++ Lab5-WPF/Lab5-WPF/UserType.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab5_WPF
+{
+    internal class UserType
+    {
+        public string privilage { get; private set; }
+        public UserType(string privilage)
+        {
+            this.privilage = privilage;
+        }
+    }
+}
